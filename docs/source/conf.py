52c0fb0301c5e8dd83f7a5d3a9c02235da57e80f
hkusklbcs <100389364+hkusklbcs@users.noreply.github.com>
2022-02-25 11:59:44 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SKL Brain & Cognitive Science'
+copyright = '2021, Wong'
+author = 'Wong'
 
 release = '0.1'
 version = '0.1.0'
